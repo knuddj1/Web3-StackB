4f894e1c366d152b873f8ce1af4bc791d0053b3f
Dean Knudson (1000025498) <KNUDDJ1@student.op.ac.nz>
2019-05-17 10:52:42 +1200

    modular CONTROLLERS

---
+++ myapp/controllers/users.js
@@ -0,0 +1,5 @@
+module.exports = {
+    index(req, res){
+        return res.status(200).send('respond with a resource');
+    }
+}
--- myapp/routes/users.js
+++ myapp/routes/users.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+const userController = require('../controllers/users');
+
+router.get('/', userController.index);
 
 module.exports = router;
